6e05b927c7746d91b0c012a9ecd3562f5b530c5a
Dario Ernesto Roca <darioernesto.roca@gmail.com>
2025-04-05 22:27:42 -0500

    first commit

---
+++ nodejs-breakpoint.js
@@ -0,0 +1,10 @@
+/* NODE.JS BREAKDOWN */
+
+const title = "JavaScript Breakdown";
+console.log(title);
+
+/* 1. What is Node.js */
+
+{
+    // Node.js is an open source, cross-platform runtime environment and library that is used for running web applications outside the client’s browser. It is used for server-side programming, and primarily deployed for non-blocking, event-driven servers, such as traditional web sites and back-end API services, but was originally designed with real-time, push-based architectures in mind. Every browser has its own version of a JS engine, and node.js is built on Google Chrome’s V8 JavaScript engine. Official sites:  https://nodejs.org/en/ - https://nodejs.org/docs/latest/api/
+}
